a4147166074029bfed2fbf2a51620b2ae0acf531
fullstackr19 <rehanankalgi7t2@gmail.com>
2021-11-06 22:43:50 +0530

    minor file fix

--- config.js
+++
@@ -1,6 +0,0 @@
-let config = {
-    API_KEY : 'at_9V42j3YNYCpHdYAyOy9itI0Ahma9c',
-    accessToken: 'pk.eyJ1IjoicmVoYW43dDIiLCJhIjoiY2t2bnAydm84MTlvaTMya2xkY3cyYWRqZyJ9._OmFrg1aS3aQ6DFg2fztXQ'
-}
-
-export default config
